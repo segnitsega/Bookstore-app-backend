1c0c0a73d44c654ca2d28856f3adb1051583d114
segnitsega <segnitsega6@gmail.com>
2025-07-08 15:07:44 +0300

    implement route to add new book

--- src/controllers/books.controllers.ts
+++ src/controllers/books.controllers.ts
@@ -7,3 +7,21 @@ export const getBooks = async(req: Request, res: Response) => {
         res.status(200).json({books: books})
     }
 }
+
+export const addBook = async(req: Request, res:Response) => {
+    const { title, author, price, stock, description, imageUrl } = req.body;
+
+    const newBook = await prisma.book.create({
+        data: {
+            title,
+            author,
+            price: parseFloat(price),
+            stock: parseInt(stock),
+            description,
+            imageUrl
+        }
+    })
+    if(newBook){
+        res.status(200).json({message: "New book successfully added.", newBook})
+    }
+}
--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,6 +1,7 @@
 import express from "express"
-import { getBooks } from "../controllers/books.controllers"
+import { getBooks, addBook } from "../controllers/books.controllers"
 
 export const bookRouter = express.Router()
 
 bookRouter.get('/', getBooks)
+bookRouter.post('/', addBook)
