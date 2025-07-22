59714a17193f22f15c5169139f27abfe6dc92653
segnitsega <segnitsega6@gmail.com>
2025-07-22 16:38:59 +0300

    adds cors to accept requests from frontend

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,16 @@ import { userRouter } from "./routes/users.routes"
 import { bookRouter } from "./routes/books.routes"
 import { errorHandler } from "./middlewares/errorHandler"
 import cartRouter from "./routes/cart.routes"
+import cors from "cors"
 
 require('dotenv').config()
 
 const port = process.env.PORT
 const server = express()
 
+server.use(cors({
+    origin:'*'
+}))
 server.use(express.json())
 server.use('/api/user', userRouter)
 server.use('/api/books', bookRouter)
