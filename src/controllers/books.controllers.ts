be796c9581bc4d66a6570422f73765c23bc86d7e
segnitsega <segnitsega6@gmail.com>
2025-07-22 08:52:36 +0300

    adds pagination to getBooks route

--- src/controllers/books.controllers.ts
+++ src/controllers/books.controllers.ts
@@ -1,39 +1,56 @@
-import prisma from "../lib/prisma"
-import { Request, Response } from "express"
-import { catchAsync } from "../utils/catchAsync"
-import { ApiError } from "../utils/apiError"
+import prisma from "../lib/prisma";
+import { Request, Response } from "express";
+import { catchAsync } from "../utils/catchAsync";
+import { ApiError } from "../utils/apiError";
 
-export const getBooks = catchAsync(async(req: Request, res: Response) => {
-    const books = await prisma.book.findMany()
-    if(!books) throw new ApiError(400, 'No books found') 
-    res.status(200).json({books: books})  
-})
+export const getBooks = catchAsync(async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.limit as string) || 10;
+  const skip = (page - 1) * limit;
 
-export const addBook = catchAsync(async(req: Request, res:Response) => {
-    const { title, author, price, stock, description, imageUrl } = req.body;
-    const newBook = await prisma.book.create({
-        data: {
-            title,
-            author,
-            price: parseFloat(price),
-            stock: parseInt(stock),
-            description,
-            imageUrl
-        }
-    })
-    if(!newBook) throw new ApiError(400, 'Error adding new book')
-    res.status(200).json({message: "New book successfully added.", newBook})
-    
-})
+  const [books, totalBooks] = await Promise.all([
+    prisma.book.findMany({
+      skip,
+      take: limit,
+    }),
+    prisma.book.count(),
+  ]);
 
-export const getBookById = catchAsync(async(req: Request, res: Response) => {
-    const id = req.params.id;
+  if (books.length === 0) throw new ApiError(400, "No books found");
+  res
+    .status(200)
+    .json({
+      books: books,
+      totalBooks: totalBooks,
+      currentPage: page,
+      totalPages: Math.ceil(totalBooks / limit),
+    });
+});
 
-    const book = await prisma.book.findUnique({
-       where: {
-        id: parseInt(id)
-       }
-    })
-    if(!book) throw new ApiError(400, 'Error finding book')
-    res.status(200).json({book: book})
-})
+export const addBook = catchAsync(async (req: Request, res: Response) => {
+  const { title, author, price, stock, description, imageUrl } = req.body;
+  const newBook = await prisma.book.create({
+    data: {
+      title,
+      author,
+      price: parseFloat(price),
+      stock: parseInt(stock),
+      description,
+      imageUrl,
+    },
+  });
+  if (!newBook) throw new ApiError(400, "Error adding new book");
+  res.status(200).json({ message: "New book successfully added.", newBook });
+});
+
+export const getBookById = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const book = await prisma.book.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  if (!book) throw new ApiError(400, "Error finding book");
+  res.status(200).json({ book: book });
+});
