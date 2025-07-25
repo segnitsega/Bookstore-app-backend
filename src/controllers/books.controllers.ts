a4fd00f81d64468fb4f22aab9576ab020c81e055
segnitsega <segnitsega6@gmail.com>
2025-07-25 15:41:21 +0300

    implements route to get featured books

--- src/controllers/books.controllers.ts
+++ src/controllers/books.controllers.ts
@@ -16,8 +16,7 @@ export const getBooks = catchAsync(async (req: Request, res: Response) => {
     prisma.book.count(),
   ]);
 
-  if (books.length === 0)
-    throw new ApiError(400, "Db working, bu No books found");
+  if (books.length === 0) throw new ApiError(400, "No books found");
   res.status(200).json({
     totalBooks: totalBooks,
     currentPage: page,
@@ -37,6 +36,37 @@ export const getBookById = catchAsync(async (req: Request, res: Response) => {
   if (!book) throw new ApiError(400, "Error finding book");
   res.status(200).json({ book: book });
 });
+
+export const getFeaturedBooks = catchAsync(
+  async (req: Request, res: Response) => {
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 4;
+    const skip = (page - 1) * limit;
+
+    const [featuredBooks, totalFeaturedBooks] = await Promise.all([
+      prisma.book.findMany({
+        where: {
+          featured: true,
+        },
+        skip,
+        take: limit,
+      }),
+      prisma.book.count({
+        where: { featured: true },
+      }),
+    ]);
+
+    if (featuredBooks.length === 0)
+      throw new ApiError(400, "No featured books found");
+
+    res.status(200).json({
+      totalfeaturedBooks: totalFeaturedBooks,
+      currentPage: page,
+      totalPage: Math.ceil(totalFeaturedBooks / limit),
+      featuredBooks: featuredBooks,
+    });
+  }
+);
 
 export const addBook = catchAsync(async (req: Request, res: Response) => {
   const { title, author, price, stock, description, imageUrl } = req.body;
--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,8 +1,10 @@
 import express from "express"
-import { getBooks, addBook, getBookById} from "../controllers/books.controllers"
+import { getBooks, addBook, getBookById, getFeaturedBooks} from "../controllers/books.controllers"
 
 export const bookRouter = express.Router()
 
 bookRouter.get('/', getBooks)
+bookRouter.get('/featured', getFeaturedBooks)
+
 bookRouter.get('/:id', getBookById)
 bookRouter.post('/', addBook)
