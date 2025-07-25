923c3ecac5aca2e8c04e26e482b7a474ee0c1c7f
segnitsega <segnitsega6@gmail.com>
2025-07-25 15:54:07 +0300

    implements route to get bestsellers books

--- src/controllers/books.controllers.ts
+++ src/controllers/books.controllers.ts
@@ -68,6 +68,35 @@ export const getFeaturedBooks = catchAsync(
   }
 );
 
+export const getBestSellers = catchAsync(
+  async (req: Request, res: Response) => {
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 10;
+    const skip = (page - 1) * limit;
+
+    const [bestsellersBooks, totalBestsellersBooks] = await Promise.all([
+      await prisma.book.findMany({
+        where: {
+          bestSellers: true,
+        },
+        skip,
+        take: limit,
+      }),
+      prisma.book.count({ where: { bestSellers: true } }),
+    ]);
+
+    if (bestsellersBooks.length === 0)
+      throw new ApiError(400, "No bestsellers book found");
+
+    res.status(200).json({
+       totalBestsellersBooks: totalBestsellersBooks,
+      currentPage: page,
+      totalPage: Math.ceil(totalBestsellersBooks / limit),
+      bestsellersBooks: bestsellersBooks,
+    })
+  }
+);
+
 export const addBook = catchAsync(async (req: Request, res: Response) => {
   const { title, author, price, stock, description, imageUrl } = req.body;
   const newBook = await prisma.book.create({
--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,10 +1,10 @@
 import express from "express"
-import { getBooks, addBook, getBookById, getFeaturedBooks} from "../controllers/books.controllers"
+import { getBooks, addBook, getBookById, getFeaturedBooks, getBestSellers} from "../controllers/books.controllers"
 
 export const bookRouter = express.Router()
 
 bookRouter.get('/', getBooks)
 bookRouter.get('/featured', getFeaturedBooks)
-
+bookRouter.get('/bestsellers', getBestSellers)
 bookRouter.get('/:id', getBookById)
 bookRouter.post('/', addBook)
