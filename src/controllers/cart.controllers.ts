bebbcd91e9055aa7f5cfc957e8725e0b8e933e07
segnitsega <segnitsega6@gmail.com>
2025-08-02 19:43:41 +0300

    implemets route to get CartItems

--- src/controllers/cart.controllers.ts
+++ src/controllers/cart.controllers.ts
@@ -4,26 +4,20 @@ import prisma from "../lib/prisma";
 import { ApiError } from "../utils/apiError";
 
 export const getCartItems = catchAsync(async (req: Request, res: Response) => {
-  const page = parseInt(req.query.page as string) || 1;
-  const limit = parseInt(req.query.limit as string) || 10;
-  const skip = (page - 1) * limit;
-
-  const [cartItems, totalItems] = await Promise.all([
-    prisma.cartItem.findMany({
-      skip,
-      take: limit,
-    }),
-    prisma.cartItem.count(),
-  ]);
-
+  const userId = parseInt(req.query.id as string);
+  const cartItems = await prisma.cartItem.findMany({
+    where: {
+      userId: userId
+    },
+    include: {
+      book: true
+    }
+  });
   if (cartItems.length === 0) throw new ApiError(400, "No items found in cart");
-
   res
     .status(200)
     .json({
-      currentPage: page,
-      totalItems,
-      totalPages: Math.ceil(totalItems / limit),
-      cartItems
+      message: "success",
+      cartItems: cartItems
     });
 });
