01f60e9fd234a90e105039a630489a10705e62ea
segnitsega <segnitsega6@gmail.com>
2025-10-20 12:10:14 +0300

    runs prisma migration

---
+++ prisma/migrations/20251020090716_removes_in_cart_boolean_property_from_book_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `inCart` on the `Book` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Book" DROP COLUMN "inCart";
